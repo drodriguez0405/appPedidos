b6fdc5fd3af97741b338b7dee30d44471731d91e
Someone54 <tomagudemac@gmail.com>
2025-03-27 12:47:01 -0500

    funciones

--- src/main/java/com/example/appPedidos/servicios/UsuarioServicio.java
+++ src/main/java/com/example/appPedidos/servicios/UsuarioServicio.java
@@ -1,11 +1,83 @@
 package com.example.appPedidos.servicios;
 
+import com.example.appPedidos.modelos.Usuario;
 import com.example.appPedidos.repositorios.IUsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioServicio {
     @Autowired
     IUsuarioRepositorio repositorio;
+
+    //Listar los metodos que activaran las consultans en la bd
+
+    //guardar
+    public Usuario guardarUsuario(Usuario datosUsuario)throws Exception{
+        try{
+            //validar los datos de entrada
+            return this.repositorio.save(datosUsuario);
+
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+    //buscar todos los registros
+    public List<Usuario> buscarTodosUsuarios()throws Exception{
+        try{
+           return this.repositorio.findAll();
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+
+    }
+
+    //buscar por id
+    public Usuario buscarUsuarioPorId(Integer idUsuario)throws Exception{
+        try{
+            Optional<Usuario> usuarioBuscado=this.repositorio.findById(idUsuario);
+            if (usuarioBuscado.isPresent()){
+                return usuarioBuscado.get();
+            }else {
+                throw new Exception("El usuario consultado no esta en BD");
+            }
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+    //modificar por id
+
+    public Usuario modificarUsuario(Integer id,Usuario datosUsuario) throws Exception{
+        try{
+            Optional<Usuario> usuarioBuscado=this.repositorio.findById(id);
+            if (usuarioBuscado.isPresent()){
+                usuarioBuscado.get().setTelefono(datosUsuario.getTelefono());
+                usuarioBuscado.get().setCorreoElectronico(datosUsuario.getCorreoElectronico());
+                return this.repositorio.save(usuarioBuscado.get());
+            }else{
+                throw new Exception("usuario no encontrado");
+            }
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //eliminar por id
+    public boolean eliminarUsuario(Integer id) throws Exception{
+        try{
+            Optional<Usuario> usuarioBuscado=this.repositorio.findById(id);
+            if (usuarioBuscado.isPresent()){
+                this.repositorio.deleteById(id);
+                return true;
+            }else{
+                throw new Exception("usuario no encontrado");
+            }
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
 }
